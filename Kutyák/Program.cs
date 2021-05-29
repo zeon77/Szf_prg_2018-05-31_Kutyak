a60054da06991c50326f9095cc39b420f2bc91fe
zeon77 <zeon77@gmail.com>
2021-05-29 10:55:56 +0200

    2-3. feladat

--- Kutyák/Program.cs
+++ Kutyák/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Kutyák
 {
@@ -10,6 +10,15 @@ namespace Kutyák
     {
         static void Main(string[] args)
         {
+            //2.
+            List<KutyaNév> kutyaNevek = new List<KutyaNév>();
+            foreach (var sor in File.ReadAllLines("KutyaNevek.csv").Skip(1))
+            {
+                kutyaNevek.Add(new KutyaNév(sor));
+            }
+
+            //3.
+            Console.WriteLine($"3. feladat: Kutyanevek száma: {kutyaNevek.Count}");
 
             Console.ReadKey();
         }
